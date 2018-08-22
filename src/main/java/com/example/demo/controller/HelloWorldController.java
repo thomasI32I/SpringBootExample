8693089867677c142c2feb47f485e3f825cef4c3
Thomas Istok <thomas.istok@gmail.com>
2018-08-22 11:07:35 +0200

    change package

---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String index() {
+        return "Hello, World!";
+        
+        //bla bla
+        
+    }
+
+}
